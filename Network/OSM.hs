6c09e6621fe19e8f25a32e1aa2017134a4d0f9c2
Thomas M. DuBuisson <Thomas.DuBuisson@gmail.com>
2012-01-14 22:24:03 -0800

    Add (untested) cacheing functionality.  Still need to expire the cache based on HTTP cache header information...

--- Network/OSM.hs
+++ Network/OSM.hs
@@ -1,8 +1,14 @@
+{-# LANGUAGE TemplateHaskell, TypeFamilies, DeriveDataTypeable, FlexibleInstances #-}
 module Network.OSM
   (  -- * Types
     TileID
   , TileCoords(..)
-     -- * High Level Operations
+  , Zoom
+    -- * High-level (cacheing) Operations
+  , getBestFitTiles
+  , getTiles
+  , getTile
+    -- * Network Operations
   , downloadBestFitTiles
   , osmTileURL
   , pixelPosForCoord
@@ -10,18 +16,33 @@ module Network.OSM
   , determineTileCoords
   , selectedTiles
   , downloadTiles
+  , downloadTile
   -- * Legal
   , copyrightText
   )where
 
 import Data.GPS
 import Network.HTTP.Conduit
+import Network.HTTP.Types (Status,statusOK)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as L
 import Control.Monad
+import Control.Monad.Base (liftBase)
 import Data.Bits
 import Data.Maybe
 import Data.Word
+
+-- For the cacheing
+import Data.Data
+import Data.Typeable
+import Data.Acid
+import Data.SafeCopy
+import qualified Data.Map as M
+import Control.Monad.State (MonadState(..))
+import Control.Monad.Reader (ask)
+import Data.Conduit
+
+type Zoom = Int
 
 -- | The official OSM tile server.
 osmTileURL :: String
@@ -37,10 +58,30 @@ data TileCoords = TileCoords
                      , maxY :: Int 
                      } 
 
-data TileID = TID { unTID :: (Int, Int) } deriving (Eq, Ord, Show)
-       
+data TileID = TID { unTID :: (Int, Int) } deriving (Eq, Ord, Show, Data, Typeable)
+
+newtype TileCache = TC (M.Map (TileID,Zoom) B.ByteString)
+  deriving (Data, Typeable)
+           
+updateTC :: ((TileID,Zoom),B.ByteString) -> Update TileCache ()
+updateTC (tid,bs) = do
+  TC tc <- get
+  let tc' = M.insert tid bs tc
+  put (TC tc')
+  
+queryTC :: (TileID,Zoom) -> Query TileCache (Maybe B.ByteString)
+queryTC tid = do
+  TC st <- ask
+  case M.lookup tid st of
+    Nothing -> return Nothing
+    Just t  -> return (Just t)
+
+$(deriveSafeCopy 1 'base ''TileID)
+$(deriveSafeCopy 1 'base ''TileCache)
+$(makeAcidic ''TileCache ['updateTC, 'queryTC])
+                                            
 -- OSM defined a of converting a coordinate and zoom level to a list of tiles
-tileNumbers :: Double -> Double -> Int -> [(Int,Int)]
+tileNumbers :: Double -> Double -> Zoom -> [(Int,Int)]
 tileNumbers latitude longitude zoom = 
              let xtile = ((longitude+180) / 360) * fromInteger (shift (1::Integer) zoom)
                  tmp = log (tan (latitude*pi / 180) + secant (latitude * pi / 180))
@@ -52,10 +93,10 @@ secant :: Floating a => a -> a
 secant a = 1 / cos a
 
 -- |Computes the rectangular map region to download based on GPS points and a zoom level
-determineTileCoords :: (Lat a, Lon a) => [a] -> Int -> Maybe TileCoords
+determineTileCoords :: (Coordinate a) => [a] -> Zoom -> Maybe TileCoords
 determineTileCoords [] _ = Nothing
 determineTileCoords wpts z =
-    let (xs,ys) = unzip $ concatMap (\w -> tileNumbers (realToFrac $ lat w) (realToFrac $ lon w) z) wpts
+    let (xs,ys) = unzip $ concatMap (\w -> tileNumbers (lat w) (lon w) z) wpts
     in Just $ TileCoords
          { maxX = maximum xs
          , minX = minimum xs
@@ -65,7 +106,7 @@ determineTileCoords wpts z =
 
 maxNumAutoTiles = 32
 
-zoomCalc :: TileCoords -> Int
+zoomCalc :: TileCoords -> Zoom
 zoomCalc tCoords = 
    let numxtiles = maxX tCoords - minX tCoords + 1
        numytiles = maxY tCoords - minY tCoords + 1
@@ -82,22 +123,36 @@ selectedTiles :: TileCoords -> [[TileID]]
 selectedTiles c = map (\j -> [TID (i,j) | i <- [minX c..maxX c]]) [minY c .. maxY c]
 
 -- | Formats the URL string
-urlStr :: String -> Int -> Int -> Int -> String
+urlStr :: String -> Int -> Int -> Zoom -> String
 urlStr base xTile yTile zoom = base ++"/"++show zoom++"/"++show xTile++"/"++show yTile++".png"
 
 -- | Takes the boundaries of the OSM tiles and downloads the tiles,
 -- keeping them in proper grid patterns for latter stiching or
 -- side-by-side display.
-downloadTiles :: String -> [[TileID]] -> Int -> IO [[B.ByteString]]
-downloadTiles base ts zoom = do
+downloadTiles :: String -> Zoom -> [[TileID]] -> IO [[Either Status B.ByteString]]
+downloadTiles base zoom ts = runResourceT $ do
+  man <- newManager
+  mapM (mapM (downloadTile' man base zoom)) ts
+  
+downloadTile :: String -> Zoom -> TileID -> IO (Either Status B.ByteString)
+downloadTile base zoom t = runResourceT $ do
+  man <- newManager
+  downloadTile' man base zoom t
+
+downloadTile' :: Manager -> String -> Zoom -> TileID -> ResourceT IO (Either Status B.ByteString)
+downloadTile' man base zoom t@(TID (x, y)) = do
   let packIt = B.concat . L.toChunks
-  mapM (mapM (\(x,y) -> liftM packIt $ simpleHttp (urlStr base x y zoom))) (map (map unTID) ts)
+  url' <- liftBase (parseUrl (urlStr base x y zoom))
+  rsp <- httpLbs url' man
+  if statusCode rsp == statusOK
+    then return (Right $ packIt (responseBody rsp))
+    else return (Left $ statusCode rsp)
 
 projectMercToLat :: Floating a => a -> a
 projectMercToLat rely = (180 / pi) * atan (sinh rely)
 
 -- | Used by @pixelPosForCoord@ for N,S,E,W coordinates for (x,y) values
-project :: Int -> Int -> Int -> (Double,Double,Double,Double)
+project :: Int -> Int -> Zoom -> (Double,Double,Double,Double)
 project x y zoom = 
   let unit = 1.0 / (2.0 ** fromIntegral zoom)
       rely1 = fromIntegral y * unit
@@ -114,11 +169,11 @@ project x y zoom =
   
 -- | Takes a WptType, the OSM tile boundaries, and a zoom level then
 -- generates (x,y) points to be placed on the Image.
-pixelPosForCoord :: (Lon a, Lat a, Integral t) => [a] -> TileCoords -> Int -> (t, t)
+pixelPosForCoord :: (Coordinate a, Integral t) => [a] -> TileCoords -> Zoom -> (t, t)
 pixelPosForCoord [] _ _ = (0,0)
 pixelPosForCoord [wpt] tCoord zoom =
-             let lat' = value $ lat wpt
-                 lon' = value $ lon wpt
+             let lat' = lat wpt
+                 lon' = lon wpt
                  tile = maximum $ tileNumbers lat' lon' zoom
                  xoffset = (fst tile - minX tCoord) * 256
                  yoffset = (snd tile - minY tCoord) * 256
@@ -139,17 +194,44 @@ copyrightText = "Tile images © OpenStreetMap (and) contributors, CC-BY-SA"
 -- The returned files should all be in an approriate grid for row/column display.
 -- See the test files of Main.hs and Main2.hs for examples of Repa stiching tiles
 -- into a single image or side by side display of individual tiles.
-downloadBestFitTiles :: (Lat a, Lon a) => String -> [a] -> IO [[B.ByteString]]
+downloadBestFitTiles :: (Coordinate a) => String -> [a] -> IO [[Either Status B.ByteString]]
 downloadBestFitTiles base points = do
+  let (coords,zoom) = bestFitCoordinates points
+      tids = selectedTiles coords
+  downloadTiles base zoom tids
+
+bestFitCoordinates :: (Coordinate a) => [a] -> (TileCoords, Zoom)
+bestFitCoordinates points = 
   let tiles = determineTileCoords points 16
       zoom = fmap zoomCalc tiles
       tiles' = join 
              . fmap (determineTileCoords points)
              $ zoom
-  case tiles' of
-     Nothing -> return []
-     Just coord -> 
-         let tids = selectedTiles coord
-         in concatMapM (downloadTiles base tids) (maybeToList zoom)
+  in case (tiles',zoom) of
+      (Just coord, Just z) -> (coord,z)
+      _                    -> (TileCoords 0 0 0 0,0)
 
-concatMapM f = liftM concat . mapM f
+getBestFitTiles :: (Coordinate a) => String -> [a] -> IO [[Either Status B.ByteString]]
+getBestFitTiles base cs = do
+  let (coords,zoom) = bestFitCoordinates cs
+      tids = selectedTiles coords
+  getTiles base tids zoom
+
+getTiles :: String -> [[TileID]] -> Zoom -> IO [[Either Status B.ByteString]]
+getTiles s ts z = mapM (mapM (\t -> getTile s t z)) ts
+
+-- FIXME constantly opening the acid state is probably really dumb.
+getTile :: String -> TileID -> Zoom -> IO (Either Status B.ByteString)
+getTile base t zoom = do
+  st <- openLocalState (TC M.empty)
+  b <- query st (QueryTC (t,zoom))
+  case b of
+    Nothing -> do
+               res <- downloadTile base zoom t
+               case res of
+                 Right bs -> do update st (UpdateTC ((t,zoom),bs))
+                                createCheckpoint st
+                                return (Right bs)
+                 Left err -> return (Left err)
+    Just x  -> return (Right x)
+
